6554e926abdffaeec1ea9283c43a0f093fde85e2
Khalid <alhajr20@gmail.com>
2020-08-31 15:25:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // mobile menu
+
+    const humb = document.querySelector('.humb'),
+          menu = document.querySelector('.menu');
+
+    humb.addEventListener('click', () => {
+        menu.classList.toggle('menu_active');
+        
+        humb.classList.toggle('menu-btn_active');
+    });
+
+    // tabs
+
+    let tabs = document.querySelectorAll('.portfolio_projects-tab'),
+        tabsContent = document.querySelectorAll('.portfolio_projects'),
+        tabsParent = document.querySelector('.portfolio_main');
+
+	// function hideTabContent() {
+        
+    //     tabsContent.forEach(item => {
+    //         item.classList.add('hideTab');
+    //         item.classList.remove('showTab', 'fade');
+    //     });
+
+    //     tabs.forEach(item => {
+    //         item.classList.remove('tab_active');
+    //     });
+	// }
+
+	// function showTabContent(i = 0) {
+    //     tabs[i].classList.add('tab_active');
+    //     tabsContent[i].classList.add('showTab', 'fade');
+    //     tabsContent[i].classList.remove('hideTab');
+    //     console.log(i);
+    // }
+    
+    // hideTabContent();
+    // showTabContent();
+
+	// tabsParent.addEventListener('click', function(event) {
+	// 	const target = event.target;
+	// 	if(target && target.classList.contains('portfolio_projects-tab')) {
+    //         tabs.forEach((item, i) => {
+    //             if (target == item) {
+    //                 hideTabContent();
+    //                 showTabContent(i);
+    //             }
+    //         });
+	// 	}
+    // });
+    
+    // modal window
+
+    const modalTrigger = document.querySelector('.aboutMe_btn'),
+          modal = document.querySelector('.resume'),
+          modalCloseBtn = document.querySelectorAll('.closeBtn');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.addEventListener('click', openModal);
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.forEach(btn => {
+        btn.addEventListener('click', closeModal);
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+});
